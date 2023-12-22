7502ee88e4b82d63a050e55271a8c3efce871e58
Dmitry Yastrebov <stewielime2018@gmail.com>
2023-12-22 22:20:07 +0300

    Create printNameTextField and make constraints

--- CoreDataMVP/Sources/Views/MainViewController.swift
+++ CoreDataMVP/Sources/Views/MainViewController.swift
@@ -4,6 +4,16 @@ class MainViewController: UIViewController {
     
     // MARK: - UI
     
+    private lazy var printNameTextField: UITextField = {
+        let textField = UITextField()
+        textField.placeholder = "Print your name here"
+        textField.textAlignment = .center
+        textField.backgroundColor = .systemGray6
+        textField.layer.cornerRadius = 15
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        return textField
+    }()
+    
     // MARK: - Lifecycle
 
     override func viewDidLoad() {
@@ -18,14 +28,23 @@ class MainViewController: UIViewController {
     
     private func setupView() {
         view.backgroundColor = .systemBackground
+        title = "Users"
+        navigationController?.navigationBar.prefersLargeTitles = true
     }
     
     private func setupHierarchy() {
-        
+        let views = [printNameTextField]
+        views.forEach { view.addSubview($0) }
     }
     
     private func setupLayout() {
-        
+        NSLayoutConstraint.activate([
+            printNameTextField.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            printNameTextField.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            printNameTextField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            printNameTextField.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            printNameTextField.heightAnchor.constraint(equalToConstant: 50)
+        ])
     }
     
     // MARK: - Action
