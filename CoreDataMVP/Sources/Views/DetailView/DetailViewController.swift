0cf0dc261a6831613fe953385b47c194fc62adbf
Dmitry Yastrebov <stewielime2018@gmail.com>
2023-12-23 18:05:19 +0300

    Create editButton and make constraints

--- CoreDataMVP/Sources/Views/DetailView/DetailViewController.swift
+++ CoreDataMVP/Sources/Views/DetailView/DetailViewController.swift
@@ -6,6 +6,27 @@ class DetailViewController: UIViewController {
     
     // MARK: - UI
     
+    private lazy var editButton: UIButton = {
+        let button = UIButton()
+        button.layer.cornerRadius = 12
+        button.layer.borderWidth = 1.5
+        button.layer.masksToBounds = true
+        
+        let buttonTitle = "Edit"
+        let attributes: [NSAttributedString.Key: Any] = [
+            .foregroundColor: UIColor.systemBlue,
+            .font: UIFont.systemFont(ofSize: 16)
+        ]
+        let attributedTitle = NSAttributedString(string: buttonTitle, attributes: attributes)
+        
+        button.setAttributedTitle(attributedTitle, for: .normal)
+        button.layer.borderColor = UIColor.systemBlue.cgColor
+        
+        button.addTarget(self, action: #selector(editButtonTapped), for: .touchUpInside)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+
     // MARK: - Lifecycle
     
     override func viewDidLoad() {
@@ -23,15 +44,25 @@ class DetailViewController: UIViewController {
     }
     
     private func setupHierarchy() {
-        
+        let views = [editButton]
+        views.forEach { view.addSubview($0) }
     }
     
     private func setupLayout() {
-        
+        NSLayoutConstraint.activate([
+            editButton.topAnchor.constraint(equalTo: view.topAnchor, constant: 60),
+            editButton.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -10),
+            editButton.widthAnchor.constraint(equalToConstant: 80),
+            editButton.heightAnchor.constraint(equalToConstant: 30)
+        ])
     }
     
     // MARK: - Action
     
+    @objc
+    func editButtonTapped() {
+        
+    }
 }
 
 // MARK: - Extensions
