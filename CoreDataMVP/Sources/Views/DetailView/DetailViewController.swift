c78adac19b1cc3cfb7aef3c30215f6a02fdeaf34
Dmitry Yastrebov <stewielime2018@gmail.com>
2023-12-23 16:23:54 +0300

    Create DetailViewController and prepare for further work

---
+++ CoreDataMVP/Sources/Views/DetailView/DetailViewController.swift
@@ -0,0 +1,35 @@
+import UIKit
+
+class DetailViewController: UIViewController {
+    
+    // MARK: - UI
+    
+    // MARK: - Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupView()
+        setupHierarchy()
+        setupLayout()
+    }
+    
+    // MARK: - Setup
+    
+    private func setupView() {
+        view.backgroundColor = .systemBackground
+    }
+    
+    private func setupHierarchy() {
+        
+    }
+    
+    private func setupLayout() {
+        
+    }
+    
+    // MARK: - Action
+    
+}
+
+// MARK: - Extensions
